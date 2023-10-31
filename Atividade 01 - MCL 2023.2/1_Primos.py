e30735b36ebae41cf74c48d40ce5d549b7b50e11
Iumy Pimentel <117225271+Iuky-O@users.noreply.github.com>
2023-10-31 20:29:26 -0300

    Update 1_Primos.py

--- Atividade 01 - MCL 2023.2/1_Primos.py
+++ Atividade 01 - MCL 2023.2/1_Primos.py
@@ -1,17 +1,33 @@
 import math
 
-Numeros = int(input("Digite a quantidade de elementos: "))
+#Função para achar numeros primos
+def acha_primos(Primos, Numeros):
+    for i in range(2, int(math.sqrt(Numeros)+1)):
+    
+      if i in Primos:
+    
+        for j in range(i**2, Numeros, i):
+    
+          if j in Primos: Primos.remove(j)
+          
+    return Primos
+    
+    
+Numeros = int(input("Digite a quantidade de elementos que vão até < que : "))
 
 
 Primos = list(range(2, Numeros))
 
-for i in range(2, int(math.sqrt(Numeros)+1)):
-
-  if i in Primos:
-
-    for j in range(i**2, Numeros, i):
-
-      if j in Primos: Primos.remove(j)
+def acha_primos(Primos, Numeros):
+    for i in range(2, int(math.sqrt(Numeros)+1)):
+    
+      if i in Primos:
+    
+        for j in range(i**2, Numeros, i):
+    
+          if j in Primos: Primos.remove(j)
+          
+    return Primos
 
 print(f"Numeros primos até {Numeros}:")
-print(Primos)
+print(acha_primos(Primos, Numeros))
