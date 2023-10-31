36991a418a15305f0d643d03c296411ababe1cf9
Iumy Pimentel <117225271+Iuky-O@users.noreply.github.com>
2023-10-31 20:48:10 -0300

    Update 1_Primos.py

--- Atividade 01 - MCL 2023.2/1_Primos.py
+++ Atividade 01 - MCL 2023.2/1_Primos.py
@@ -18,16 +18,5 @@ Numeros = int(input("Digite a quantidade de elementos que vão até < que : "))
 
 Primos = list(range(2, Numeros))
 
-def acha_primos(Primos, Numeros):
-    for i in range(2, int(math.sqrt(Numeros)+1)):
-    
-      if i in Primos:
-    
-        for j in range(i**2, Numeros, i):
-    
-          if j in Primos: Primos.remove(j)
-          
-    return Primos
-
 print(f"Numeros primos < {Numeros}:")
 print(acha_primos(Primos, Numeros))
